f77683b0aef634297fa0226091cd30cf44c2f3c0
Aritra Naharay <aritra.naharay@k12onlineschools.com>
2026-02-07 03:02:12 +0530

    removing files

--- utils/connections/jwt/getTokens.jwt.js
+++
@@ -1,8 +0,0 @@
-const GetAccessToken = (request) => {
-    const authHeader = request.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1];
-
-    return token;
-}
-
-module.exports = { GetAccessToken };
